a8a9c8e53461583a9f94dbb8b95c86de0ec5a2bc
Min Kyu Han <minkyu.han1029@gmail.com>
2020-11-19 18:17:05 -0500

    Steps work but back edge doesn't render

--- public/texture/Main.js
+++ public/texture/Main.js
@@ -33,4 +33,61 @@ Main.prototype.loadSpriteSheet = function() {
 Main.prototype.spriteSheetLoaded = function() {
   this.scroller = new Scroller(this.stage)
   requestAnimationFrame(this.update.bind(this))
+
+  this.pool = new WallSpritesPool()
+  this.wallSlices = []
+}
+
+Main.prototype.generateTestWallSpan = function() {
+  var lookupTable = [
+    this.pool.borrowFrontEdge, // 1st slice
+    this.pool.borrowWindow, // 2nd slice
+    this.pool.borrowDecoration, // 3rd slice
+    this.pool.borrowWindow, // 4th slice
+    this.pool.borrowDecoration, // 5th slice
+    this.pool.borrowWindow, // 6th slice
+    this.pool.borrowBackEdge // 7th slice
+  ]
+  var yPos = [
+    128, // 1st slice
+    128, // 2nd slice
+    128, // 3rd slice
+    192, // 4th slice
+    192, // 5th slice
+    192 // 6th slice
+  ]
+
+  for (var i = 0; i < lookupTable.length; i++) {
+    var func = lookupTable[i]
+
+    var sprite = func.call(this.pool)
+    sprite.position.x = 64 + i * 64
+    sprite.position.y = yPos[i]
+
+    this.wallSlices.push(sprite)
+
+    this.stage.addChild(sprite)
+  }
+}
+
+Main.prototype.clearTestWallSpan = function() {
+  var lookupTable = [
+    this.pool.returnFrontEdge, // 1st slice
+    this.pool.returnWindow, // 2nd slice
+    this.pool.returnDecoration, // 3rd slice
+    this.pool.returnWindow, // 4th slice
+    this.pool.returnDecoration, // 5th slice
+    this.pool.returnWindow, // 6th slice
+    this.pool.returnBackEdge // 7th slice
+  ]
+
+  for (var i = 0; i < lookupTable.length; i++) {
+    var func = lookupTable[i]
+    var sprite = this.wallSlices[i]
+
+    this.stage.removeChild(sprite)
+    func.call(this.pool, sprite)
+  }
+
+  this.wallSlices = []
 }
